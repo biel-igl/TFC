43a6a3053fe599b34f0245ec187462d15707cf29
biel-igl <gabrielpires524@gmail.com>
2023-06-21 22:02:55 -0300

    arruma req

--- app/backend/src/utils/mapStatusHTTP.ts
+++ app/backend/src/utils/mapStatusHTTP.ts
@@ -2,6 +2,7 @@ export default function mapStatusHTTP(status: string): number {
   switch (status) {
     case 'SUCCESSFUL': return 200;
     case 'UPDATE': return 201;
-    default: return 404;
+    case 'UNAUTHORIZED': return 401;
+    default: return 500;
   }
 }
